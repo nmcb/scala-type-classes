a785eeec41c6f5450045f4b77365dc07728f7fdb
nmcb <nmcb@xs4all.nl>
2026-04-14 10:53:06 +0200

    move implicit conversion to domain.scala

--- src/main/scala/playground/Main.scala
+++ src/main/scala/playground/Main.scala
@@ -4,7 +4,6 @@ package playground
 object Main extends App {
 
   // depicts the usage of an existing label maker type-class
-  import LabelMaker._
   def label(t: Address) = implicitly[LabelMaker[Address]].toLabel(t)
   println(label(new Address(66, "Route", "Brussels", "BXL", "1000")))
 
@@ -13,11 +12,11 @@ object Main extends App {
   println(new Address(101, "Types", "Amsterdam", "NH", "1000AA").label)
 
   // depicts the usage of a label maker type-class employing ad-hoc polymorphism)
-  implicit val personLabelMaker = new LabelMaker[Person] {
+  implicit val personLabelMaker: LabelMaker[Person] = new LabelMaker[Person] {
     override def toLabel(person: Person): String = {
       import person._
       "%s %s, %s [%s]".format(firstName, lastName, title.getOrElse(""), nick)
     }
   }
-  println(new Person("Jon", "Osterman", Some("Dr."), "Manhattan").label)
+  println(Person("Jon", "Osterman", Some("Dr."), "Manhattan").label)
 }
